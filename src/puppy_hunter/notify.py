521673f0ba35b6a9c1c608baf0bdf8d7fce632e5
Chris Brodt <chris@uberbrodt.net>
2020-05-23 21:31:28 -0500

    make email addresses a variable

--- src/puppy_hunter/notify.py
+++ src/puppy_hunter/notify.py
@@ -10,13 +10,14 @@ Send out updated puppies since unix time
 
 
 def updated_puppies_since(time, db_name):
-    msg = EmailMessage()
     now = datetime.now()
     now_s = now.strftime("%b-%d %H:%M")
     puppies = puppy_hunter.db.get_updated_since(db_name, time)
 
-    smtp_user = os.getenv("PUPPYHUNTER_SMTP_USER", "chris@uberbrodt.net")
+    smtp_user = os.environ.get("PUPPYHUNTER_SMTP_USER")
     smtp_passwd = os.environ.get("PUPPYHUNTER_SMTP_PASSWD")
+    # expected to be a string of email addresses seperated by ","
+    sendto = os.environ.get("PUPPYHUNTER_SENDTO").split(",")
 
     msg_content = ""
 
@@ -34,14 +35,17 @@ def updated_puppies_since(time, db_name):
         updated_at: {pupper['updated_at']}
         -------------------------------------------
         """
-    msg.set_content(msg_content)
+    if msg_content != "":
+        print("Got PUPdates! Sending notifications")
+        msg = EmailMessage()
+        msg.set_content(msg_content)
 
-    msg["Subject"] = f"Puppy Hunter PUPdate! - {now_s}"
-    msg["From"] = "puppyhunter@uberbrodt.net"
+        msg["Subject"] = f"Puppy Hunter PUPdate! - {now_s}"
+        msg["From"] = "puppyhunter@uberbrodt.net"
 
-    smtp = smtplib.SMTP("smtp.gmail.com", 587)
-    smtp.starttls()
-    smtp.login(smtp_user, smtp_passwd)
-    smtp.send_message(msg, to_addrs=["c.brodt@gmail.com", "chris@uberbrodt.net"])
+        smtp = smtplib.SMTP("smtp.gmail.com", 587)
+        smtp.starttls()
+        smtp.login(smtp_user, smtp_passwd)
+        smtp.send_message(msg, to_addrs=sendto)
 
 
